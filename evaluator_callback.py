5618d895bcb80a281c263c92ea7b9f83bc5568e5
wq <smilingwang2@gmail.com>
2020-04-04 20:42:11 +0800

    continuation pass style

--- evaluator_callback.py
+++ evaluator_callback.py
@@ -3,24 +3,32 @@
 from environment import Environment
 from utils import apply_op
 from itertools import zip_longest
+from parse import Parser
+from token_stream import TokenStream
+from input_stream import InputStream
+from typing import Dict, Callable, Any
+import time
+
+Ast = Dict
 
 
-def evaluate(ast: dict, env: Environment, callback):
-    type = ast['type']
-    if type in {'num', 'str', 'bool'}:
+def evaluate(ast: Ast, env: Environment, callback) -> None:
+    type_ = ast['type']
+    if type_ in {'num', 'str', 'bool'}:
         callback(ast['value'])
-    elif type == 'var':
+    elif type_ == 'var':
         callback(env.get(ast['value']))
-    elif type == 'assign':
+    elif type_ == 'assign':
         if ast['left']['type'] != 'var':
             raise Exception(f"Cannot assign to {ast['left']}")
-        evaluate(ast['right'], env, lambda right: callback(env.set(ast['left']['value'], right)))
-    elif type == 'binary':
+        evaluate(ast['right'], env, lambda right: callback(
+            env.set(ast['left']['value'], right)))
+    elif type_ == 'binary':
         evaluate(ast['left'], env, lambda left: evaluate(ast['right'], env, lambda right: callback(
             apply_op(ast['operator'], left, right))))
-    elif type == 'lambda':
+    elif type_ == 'lambda':
         callback(make_lambda(env, ast))
-    elif type == 'if':
+    elif type_ == 'if':
         def if_callback(cond):
             if cond is not False:
                 evaluate(ast['then'], env, callback)
@@ -30,7 +38,7 @@ def evaluate(ast: dict, env: Environment, callback):
                 callback(False)
 
         evaluate(ast['cond'], env, if_callback)
-    elif type == 'let':
+    elif type_ == 'let':
         def loop(env, i):
             if i < len(ast['vars']):
                 var = ast['vars'][i]
@@ -48,7 +56,7 @@ def evaluate(ast: dict, env: Environment, callback):
                 evaluate(ast['body'], env, callback)
 
         loop(env, 0)
-    elif type == 'prog':
+    elif type_ == 'prog':
         def loop(last, i):
             if i < len(ast['prog']):
                 evaluate(ast['prog'][i], env, lambda value: loop(value, i + 1))
@@ -56,7 +64,7 @@ def evaluate(ast: dict, env: Environment, callback):
                 callback(last)
 
         loop(False, 0)
-    elif type == 'call':
+    elif type_ == 'call':
         def call_callback(func):
             def loop(args, i):
                 def arg_callback(arg):
@@ -72,19 +80,58 @@ def evaluate(ast: dict, env: Environment, callback):
 
         evaluate(ast['func'], env, call_callback)
     else:
-        raise Exception("I don'tt know how to evaluate " + ast['type'])
+        raise Exception(f"I don'tt know how to evaluate {ast['type']}")
 
 
-def make_lambda(env, exp):
+def make_lambda(env: Environment, ast: Ast) -> Callable:
     def lambda_function(callback, *args):
-        scope = env.extend()
-        names = exp['vars']
+        names = ast['vars']
         assert len(names) >= len(args)
+        scope = env.extend()
         for name, value in zip_longest(names, args, fillvalue=False):
             scope.define(name, value)
-        evaluate(exp['body'], scope, callback)
+        evaluate(ast['body'], scope, callback)
 
-    if exp['name']:
+    if ast['name']:
         env = env.extend()
-        env.define(exp['name'], lambda_function)
+        env.define(ast['name'], lambda_function)
     return lambda_function
+
+
+def main():
+    # code = "sum = lambda(x, y) x + y; print(sum(2, 3));"
+    code = """
+    fib = λ(n) if n < 2 then n else fib(n - 1) + fib(n - 2);
+    time( λ() println(fib(6)) );
+    """
+    # code = """
+    # fib = λ(n) if n < 2 then n else fib(n - 1) + fib(n - 2);
+    # println(fib(8));
+    # """
+    parser = Parser(TokenStream(InputStream(code)))
+    global_env = Environment()
+
+    def custom_print(callback, txt):
+        print(txt, end=' ')
+        callback(False)
+
+    def custom_println(callback, txt):
+        print(txt, end='\n')
+        callback(False)
+
+    def timing(callback, func):
+        start_time = time.time()
+
+        def timing_callback(result):
+            end_time = time.time()
+            print(f"Time: {(end_time - start_time) * 1000}ms", end='\n')
+            callback(result)
+        func(timing_callback)
+    global_env.define("print", custom_print)
+    global_env.define("time", timing)
+    global_env.define("println", custom_println)
+    evaluate(parser(), global_env, lambda result: result)
+
+
+if __name__ == "__main__":
+    main()
